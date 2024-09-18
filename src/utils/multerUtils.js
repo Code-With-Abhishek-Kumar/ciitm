25727f616bfe5f254c1cae01540d4881eb3709f7
Abhishek Kumar <abhishek9661342993@gmail.com>
2024-09-18 15:31:51 +0530

    modified Album

--- src/utils/multerUtils.js
+++ src/utils/multerUtils.js
@@ -9,7 +9,7 @@ console.log(path.dirname);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/home/codewa/Desktop/myapp/uploads");
+    cb(null, "public/upload/");
   },
   filename: function (req, file, cb) {
     crypto.randomBytes(12 , function(error, buffer){
